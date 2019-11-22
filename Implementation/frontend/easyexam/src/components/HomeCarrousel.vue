cbb4b8331600bd02b86d604871995b48cd1da898
anggieyc <anggie.yengle@utec.edu.pe>
2019-11-22 07:35:24 -0500

    Welcome style

--- Implementation/frontend/easyexam/src/components/HomeCarrousel.vue
+++ Implementation/frontend/easyexam/src/components/HomeCarrousel.vue
@@ -37,14 +37,6 @@
                     >
                 </template>
             </b-carousel-slide>
-
-            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-                    a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-                </p>
-            </b-carousel-slide>
         </b-carousel>
     </div>
 </template>
