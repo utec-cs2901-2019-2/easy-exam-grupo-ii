8a1ecfc7447de66c2e1748c55a924c72b5128821
giordano.alvitez@utec.edu.pe <giordano.alvitez@utec.edu.pe>
2019-11-21 19:03:01 -0500

    Generate Exam

--- Implementation/backend/backend/src/main/java/com/easyexam/controller/ExamController.java
+++ Implementation/backend/backend/src/main/java/com/easyexam/controller/ExamController.java
@@ -2,16 +2,26 @@ package com.easyexam.controller;
 
 import com.easyexam.model.aux.ApiResponse;
 import com.easyexam.model.aux.ExamCompleted;
-import com.easyexam.model.aux.ProblemCompleted;
 import com.easyexam.service.IExamService;
 import com.easyexam.service.impl.ProblemServiceImpl;
+import de.nixosoft.jlr.JLRConverter;
+import de.nixosoft.jlr.JLRGenerator;
+import de.nixosoft.jlr.JLROpener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.jpa.domain.JpaSort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.*;
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -24,12 +34,85 @@ public class ExamController {
     @Autowired
     IExamService examService;
 
+    String path="./src/main/java/com/easyexam/files";
+    String separator="/";
+
     @PostMapping("/submitExam")
     public ApiResponse<ExamCompleted> register(@Valid @RequestBody ExamCompleted examCompleted) {
         LOG.info("Entro a submit problem");
         LOG.info(examCompleted.getTitle());
         examService.saveExam(examCompleted);
         return new ApiResponse<>(200, "success", true);
+    }
+
+    @GetMapping("/generateExam")
+    public void generateExam(HttpServletResponse response) throws IOException {
+        LOG.info("Entro a generate Exam");
+        String name="prueba.tex";
+        String namePDF="prueba.pdf";
+        generateLatex(name);
+        generatePDF(namePDF,name);
+        response.setContentType("application/pdf");
+
+        response.setHeader("Content-Disposition","attachment;filename="+path+separator+"prueba.pdf");
+        response.setHeader("Content-Transfer-Enconding","binary");
+        try{
+            BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
+            FileInputStream fis = new FileInputStream(path+separator+"prueba.pdf");
+            int len;
+            byte[] buf = new byte[1024];
+            while((len = fis.read(buf)) > 0) {
+                bos.write(buf,0,len);
+            }
+            bos.close();
+            response.flushBuffer();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private void generateLatex(String name) throws IOException {
+
+        String documentclass = "\\documentclass{article}";
+        String begin = "\\begin{document}";
+        String data = "Helloworld";
+        String end = "\\end{document}";
+
+        String path="./src/main/java/com/easyexam/files";
+        String separator="/";
+
+        FileOutputStream out = new FileOutputStream(path+separator+name);
+
+        out.write(documentclass.getBytes());
+        out.write(begin.getBytes());
+        out.write(data.getBytes());
+        out.write(end.getBytes());
+        out.close();
+    }
+
+    private void generatePDF(String name,String fileLatex) throws IOException {
+
+        String path="./src/main/java/com/easyexam/files";
+        String separator="/";
+
+
+        File workingDirectory = new File(path);
+
+        File template = new File(workingDirectory.getAbsolutePath() + separator + fileLatex);
+
+        //File tempDir = new File(workingDirectory.getAbsolutePath() + File.separator + "temp");
+
+        JLRGenerator pdfGen = new JLRGenerator();
+        pdfGen.deleteTempFiles(false,false,false);
+
+        if (!pdfGen.generate(template, workingDirectory, workingDirectory)) {
+            System.out.println(pdfGen.getErrorMessage());
+        }
+        //JLROpener.open(pdfGen.getPDF());
+
     }
 
 }
